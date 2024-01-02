5e4467bdab9de1a55a97a6e12f4640d82845d88a
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2024-01-02 16:18:12 +0530

    Project Setup | ENV | Appwrite | Tailwind CSS
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">A blog app with appwrite</h1>
+    </>
+  );
+}
+
+export default App;
