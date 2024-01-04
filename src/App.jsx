a31f559aba15f0501bbe2a836bbb4f9c16ff9a94
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2024-01-04 19:18:28 +0530

    configure redux toolkit
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold">A blog app with appwrite</h1>
-    </>
-  );
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-200">
+      <div className="w-full block">
+        <Header />
+        <main> {/* TODO:  <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
